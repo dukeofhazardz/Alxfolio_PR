20a58ca99d9ba4cc6f5eca49f8c6a1d5ab718346
Daniel John <dj71286@gmail.com>
2023-12-10 20:53:32 +0100

    Refactored Code

--- functions.py
+++ functions.py
@@ -8,6 +8,7 @@ from github import Github
 from redis import Redis
 from datetime import timedelta
 
+URL = "https://api.github.com"
 redis_client = Redis(host='localhost', port=6379, db=0)
 
 def get_user(username):
@@ -15,7 +16,7 @@ def get_user(username):
     user = redis_client.get(f"{username}")
     if user is None:
         try:
-            g = Github(base_url="https://api.github.com")
+            g = Github(base_url=URL)
             user_dict = vars(g.get_user(username))
             user = {key : value for key, value in user_dict.items() if key.startswith('_rawData')}
             user = user['_rawData']
@@ -32,7 +33,7 @@ def validate_alx(username):
     repo = redis_client.get(f"{username}"+"_repo")
     if repo is None:
         try:
-            g = Github(base_url="https://api.github.com")
+            g = Github(base_url=URL)
             check_repo_exists = "alx-system_engineering-devops"
             repo = g.get_repo(f"{username}/{check_repo_exists}")
             if repo:
@@ -47,7 +48,7 @@ def get_all_repos(username):
     all_repos = redis_client.get(f"{username}"+"_all_repos")
     if all_repos is None:
         try:
-            g = Github(base_url="https://api.github.com")
+            g = Github(base_url=URL)
             all_repos = []
             user = g.get_user(username)
             repos = user.get_repos()
